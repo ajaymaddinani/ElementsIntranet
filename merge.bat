ca901c1a9c07411555ca4b9fabff293526db697a
“Ajay” <ajaybabu4u06@gmail.com>
2020-07-06 14:44:15 +0800

    checkout

--- merge.bat
+++ merge.bat
@@ -1,4 +1,5 @@
-git push https://edb.ajaybabu:yjE_uVPi5z3VRxB8svVK@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git HEAD: mirror-primry
+git checkout https://edb.ajaybabu:yjE_uVPi5z3VRxB8svVK@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git 
+git push https://edb.ajaybabu:yjE_uVPi5z3VRxB8svVK@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git 
 
 
 
