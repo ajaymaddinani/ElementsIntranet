b7637cde6b7a663177283642350a4073381806d6
“Ajay” <ajaybabu4u06@gmail.com>
2020-07-06 13:56:42 +0800

    remove mirror

--- merge.bat
+++ merge.bat
@@ -1,4 +1,4 @@
-git push --mirror https://ajaymaddinani:1a1b44acd3ff5f4ad4dae02fcf919aec677006f2@github.com/ajaymaddinani/ElementsIntranet.git
+git push https://ajaymaddinani:1a1b44acd3ff5f4ad4dae02fcf919aec677006f2@github.com/ajaymaddinani/ElementsIntranet.git
 
 
 
