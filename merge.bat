5080233f7dab01f528cefd47aa63fd1ef6c92ef1
“Ajay” <ajaybabu4u06@gmail.com>
2020-07-06 11:36:06 +0800

    change to clone

--- merge.bat
+++ merge.bat
@@ -1,1 +1,1 @@
-git push --mirror https://R69_cR_54KNQHahm3QGQ@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git
+git clone https://R69_cR_54KNQHahm3QGQ@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git
