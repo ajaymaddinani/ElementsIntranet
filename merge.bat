3ab9a2ba86bbf034869540406d77bf658e85f8f3
“Ajay” <ajaybabu4u06@gmail.com>
2020-07-06 12:19:30 +0800

    Add Username and pwd

--- merge.bat
+++ merge.bat
@@ -1,1 +1,1 @@
-git push --mirror https://R69_cR_54KNQHahm3QGQ@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git
+git push --mirror https://edb.ajaybabu:R69_cR_54KNQHahm3QGQ@rocket01.galaxy.edb.gov.sg/edb.ajaybabu/mirror-primry.git
