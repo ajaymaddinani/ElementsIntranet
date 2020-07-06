385f8a98fd9350442f45faa9a984ffcc4bd3550f
“Ajay” <ajaybabu4u06@gmail.com>
2020-07-06 14:02:34 +0800

    pwd update

--- merge.bat
+++ merge.bat
@@ -1,1 +1,4 @@
- git push https://ajaymaddinani:1a1b44acd3ff5f4ad4dae02fcf919aec677006f2@github.com/ajaymaddinani/ElementsIntranet.git HEAD:ElementsIntranet
+git push https://ajaymaddinani:1a1b44acd3ff5f4ad4dae02fcf919aec677006f2@github.com/ajaymaddinani/ElementsIntranet.git HEAD:ElementsIntranet
+
+
+
